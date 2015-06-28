371f943e76fbf4cdfc9992016c78f8e77620f463
Bradley Conn <bec5030@rit.edu>
2015-06-27 23:53:45 -0400

    Made great progress. Now I have to figure out what the usbd_conf.h really is...

--- kern/keyboard.c
+++ kern/keyboard.c
@@ -8,37 +8,81 @@
 
 void usb_init(void) {
     USBD_HandleTypeDef USBD_Handle = {0};
-    //uint8_t                 id;
-    //uint32_t                dev_config;
-    //uint32_t                dev_default_config;
-    //uint32_t                dev_config_status; 
-    //USBD_SpeedTypeDef       dev_speed; 
-    //USBD_EndpointTypeDef    ep_in[15];
-    //USBD_EndpointTypeDef    ep_out[15];  
-    //uint32_t                ep0_state;  
-    //uint32_t                ep0_data_len;     
-    //uint8_t                 dev_state;
-    //uint8_t                 dev_old_state;
-    //uint8_t                 dev_address;
-    //uint8_t                 dev_connection_status;  
-    //uint8_t                 dev_test_mode;
-    //uint32_t                dev_remote_wakeup;
+        //uint8_t                 id;
+        //uint32_t                dev_config;
+        //uint32_t                dev_default_config;
+        //uint32_t                dev_config_status; 
+        //USBD_SpeedTypeDef       dev_speed; 
+        //USBD_EndpointTypeDef    ep_in[15];
+        //USBD_EndpointTypeDef    ep_out[15];  
+        //uint32_t                ep0_state;  
+        //uint32_t                ep0_data_len;     
+        //uint8_t                 dev_state;
+        //uint8_t                 dev_old_state;
+        //uint8_t                 dev_address;
+        //uint8_t                 dev_connection_status;  
+        //uint8_t                 dev_test_mode;
+        //uint32_t                dev_remote_wakeup;
 
-    //USBD_SetupReqTypedef    request;
-    //USBD_DescriptorsTypeDef *pDesc;
-    USBD_Handle.pClass = &USBD_HID;
-    //void                    *pClassData;  
-    //void                    *pUserData;    
-    //void                    *pData;    
+        //USBD_SetupReqTypedef    request;
+        //USBD_DescriptorsTypeDef *pDesc;
+        //USBD_ClassTypeDef       *pClass;
+        //void                    *pClassData;  
+        //void                    *pUserData;    
+        //void                    *pData;    
     USBD_DescriptorsTypeDef USBD_Descriptor;
+        /* uint8_t  *(*GetDeviceDescriptor)( USBD_SpeedTypeDef speed , uint16_t *length); */  
+        /* uint8_t  *(*GetLangIDStrDescriptor)( USBD_SpeedTypeDef speed , uint16_t *length); */ 
+        /* uint8_t  *(*GetManufacturerStrDescriptor)( USBD_SpeedTypeDef speed , uint16_t *length); */  
+        /* uint8_t  *(*GetProductStrDescriptor)( USBD_SpeedTypeDef speed , uint16_t *length); */  
+        /* uint8_t  *(*GetSerialStrDescriptor)( USBD_SpeedTypeDef speed , uint16_t *length); */  
+        /* uint8_t  *(*GetConfigurationStrDescriptor)( USBD_SpeedTypeDef speed , uint16_t *length); */  
+        /* uint8_t  *(*GetInterfaceStrDescriptor)( USBD_SpeedTypeDef speed , uint16_t *length); */ 
     uint8_t id = 11;
     USBD_StatusTypeDef result = USBD_Init(&USBD_Handle, &USBD_Descriptor, id);
     if (result !=  USBD_OK) {
-        puts("baaad");
+        puts("bad init");
     }
     else {
         puts("good init");
     }
+    /* Add Supported Class */
+    USBD_RegisterClass(&USBD_Handle, USBD_HID_CLASS);
+    if (result !=  USBD_OK) {
+        puts("bad init");
+    }
+    else {
+        puts("good init");
+    }
+    /* Start Device Process */
+    USBD_Start(&USBD_Handle);
+    if (result !=  USBD_OK) {
+        puts("bad init");
+    }
+    else {
+        puts("good init");
+    }
+    USBD_SetupReqTypedef request = {0};
+    request.bRequest = USB_REQ_SET_ADDRESS;
+    request.wValue = 0x11;
+    result =  USBD_StdDevReq (&USBD_Handle, &request);
+    if (result !=  USBD_OK) {
+        puts("bad init");
+    }
+    else {
+        puts("good init");
+    }
+    request.bRequest = USB_REQ_SET_CONFIGURATION;
+    request.wValue = 0x1;
+    result =  USBD_StdDevReq (&USBD_Handle, &request);
+    if (result !=  USBD_OK) {
+        puts("bad init");
+    }
+    else {
+        puts("good init");
+    }
+    uint8_t report[6] = "HELLO";
+    USBD_HID_SendReport(&USBD_Handle, report,6);
 
 //    USBD_HID_SendReport();
 //uint8_t USBD_HID_SendReport     (USBD_HandleTypeDef  *pdev, 
